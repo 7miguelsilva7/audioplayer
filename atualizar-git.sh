6618d61c05635814889266cb05e687c1e8b96810
7miguelsilva7 <7miguelsilva7@gmail.com>
2018-07-17 22:32:54 -0300

    update

--- atualizar-git.sh
+++ atualizar-git.sh
@@ -4,4 +4,9 @@ git add --all
 git commit -m "update"
 git push
 
-chromium-browser https://cpanel.hostinger.com.br/git/index/aid/29500595
+if [ -e /opt/lampp/htdocs/ssh-audioPlayer.sh ]; then 
+/opt/lampp/htdocs/ssh-audioPlayer.sh;
+
+else 
+chromium-browser https://cpanel.hostinger.com.br/git/index/aid/29500595; 
+fi

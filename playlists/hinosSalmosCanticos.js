735f87530a7caf74b4c7e8b99c7b0337f00bf59f
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-17 22:08:03 -0300

    update

--- playlists/hinosSalmosCanticos.js
+++ playlists/hinosSalmosCanticos.js
@@ -118,6 +118,7 @@ jQuery(function ($) {
                 {"track":	90	,	 "voz": "José Batista Pereira",	 "name": "VEM JESUS NOS BUSCAR",	 "length": "	03:01	",	 "file": "	https://www.mediafire.com/file/882i0mxtm544b4o/	"},
                 {"track":	91	,	 "voz": "José Batista Pereira",	 "name": "SALMO 23 - VERSÃO II",	 "length": "	03:24	",	 "file": "	https://www.mediafire.com/file/83vb9lj55ackjyr/	"},
                 {"track":	92	,	 "voz": "José Batista Pereira",	 "name": "SUBSTITUiÇÃO",	 "length": "	03:09	",	 "file": "	https://www.mediafire.com/file/ff6j2pycylznawa/	"},
+                {"track":	93	,	 "voz": "José Batista Pereira",	 "name": "UMA VEZ MAIS",	 "length": "	03:01	",	 "file": "	https://www.mediafire.com/file/ihnederz4peages/	"},
 
 ],
             buildPlaylist = $.each(tracks, function(key, value) {

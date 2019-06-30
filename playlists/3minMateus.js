1b618e8acb942ebd9c5775faf779cfcd9b9e7865
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-29 21:35:07 -0300

    update

--- playlists/3minMateus.js
+++ playlists/3minMateus.js
@@ -337,7 +337,6 @@ if(localStorage.hasOwnProperty(audioNextKey[0].innerHTML) & a == undefined)
   {
     //initialize plyr
     plyr.setup($('#audio1'), {});
-                        loadTrack(index)
 
   }
 // }

cc1a83fca3401febbe4eba3b8f5aa3d90a737c74
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-08-17 00:10:12 -0300

    update

--- playlists/musicTudo.js
+++ playlists/musicTudo.js
@@ -483,6 +483,6 @@ var ended = window.location.href.split("#").length;
 if (ended > 1)
 {
     localStorage.setItem(audioNextKey[0].innerHTML, '#' + 0);
-    // location.replace(localStorage.getItem(audioNextKey[0].innerHTML))
+    location.replace(localStorage.getItem(audioNextKey[0].innerHTML + '#' + 0))
 
 }

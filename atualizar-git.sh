88fe39ddc4b9df7ef8bff7c828943c5e6a24c858
Miguel Silva <7miguelsilva7@gmail.com>
2021-06-26 14:49:31 -0300

    update

--- atualizar-git.sh
+++ atualizar-git.sh
@@ -4,15 +4,15 @@ git add --all
 git commit -m "update"
 git push
 
-ssh -t u378308740@153.92.6.22 -p 65002 "
+ssh -t u378308740@153.92.7.151 -p 65002 "
 cd public_html/audioplayer && 
 bash resetGit
 "
 
 
-if [ -e /opt/lampp/htdocs/ssh-audioPlayer.sh ]; then 
-/opt/lampp/htdocs/ssh-audioPlayer.sh;
+#if [ -e /opt/lampp/htdocs/ssh-audioPlayer.sh ]; then 
+#/opt/lampp/htdocs/ssh-audioPlayer.sh;
 
-else 
-chromium-browser https://cpanel.hostinger.com.br/git/index/aid/29500595; 
-fi
+#else 
+#chromium-browser https://cpanel.hostinger.com.br/git/index/aid/29500595; 
+#fi
